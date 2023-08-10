7d3b0911989f045adc77f98c20c44e6c793a9383
sravanthithatipally <126408578+sravanthithatipally@users.noreply.github.com>
2023-08-11 02:37:04 +0530

    Add files via upload

---
+++ selectpet-js45.js
@@ -0,0 +1,16 @@
+let petsImageUrls = {
+    dog: "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/select-your-pet-dog-img.png",
+    cat: "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/select-your-pet-cat-img.png",
+    parrot: "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/select-your-pet-parrot-img.png",
+    spider: "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/select-your-pet-spider-img.png",
+    rabbit: "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/select-your-pet-rabbit-img.png"
+};
+let selectele = document.getElementById("petSelect");
+let imageele = document.getElementById("petImg");
+
+function optionchanged(event) {
+    let imagename = event.target.value;
+    imageele.setAttribute("src", petsImageUrls[imagename]);
+
+}
+selectele.addEventListener("change", optionchanged);
